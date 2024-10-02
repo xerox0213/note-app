b4d70ee59686aab116f021b95d26533b68272825
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-10-02 13:34:10 +0200

    feat: création du composant Header

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import SearchBar from "./SearchBar.vue";
+</script>
+
+<template>
+  <header class="grid grid-rows-2 md:grid-cols-3 md:grid-rows-1 items-center p-2.5 border-b border-b-gray-200">
+    <a href="/" class="justify-self-center md:justify-self-start text-xl font-bold">Notes app</a>
+    <SearchBar />
+  </header>
+</template>
+
+<style scoped>
+
+</style>
