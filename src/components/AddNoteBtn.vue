5c8941484a26361ad83ac7c7c9b9e424ca4c40fb
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-10-02 21:03:13 +0200

    feat: ouverture du composant EditNoteModale en mode ajout

--- src/components/AddNoteBtn.vue
+++ src/components/AddNoteBtn.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
 import {PlusIcon} from "@heroicons/vue/24/solid";
+import {useEditModal} from "../store/edit-modal-store.ts";
+import {EditType} from "../ts/enums.ts";
+
+const editModalStore = useEditModal()
+
+const displayEditModal = () => {
+  editModalStore.displayEditModal(-1, EditType.ADD)
+}
 </script>
 
 <template>
-  <button class="fixed right-8 bottom-8 p-2 bg-blue-500 rounded-lg transition-colors hover:bg-blue-600">
+  <button @click="displayEditModal"
+          class="fixed right-8 bottom-8 p-2 bg-blue-500 rounded-lg transition-colors hover:bg-blue-600">
     <PlusIcon class="size-9 text-white"/>
   </button>
 </template>
