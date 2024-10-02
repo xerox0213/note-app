0d0b3a45eeab887215662cdbfe25139f2c24b8c0
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-10-02 13:33:24 +0200

    feat: création du composant SearchBar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import {MagnifyingGlassIcon} from "@heroicons/vue/24/outline"
+import {useSearchNote} from "../store/search-note-store.ts"
+
+const searchNoteStore = useSearchNote()
+</script>
+
+<template>
+  <div class="flex items-center bg-gray-100 px-4 py-2 rounded">
+    <input v-model="searchNoteStore.inputUserSearch"
+           type="text" placeholder="Search notes"
+           class="grow bg-transparent rounded outline-0"/>
+    <MagnifyingGlassIcon class="size-4 text-gray-500"/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
