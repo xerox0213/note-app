774840ca3aa7f3645a944318bb8a10b12dfc1d3c
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-10-02 21:01:04 +0200

    feat: création du composant EditNoteModal

---
+++ src/components/EditNoteModal.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+import Modal from "./Modal.vue";
+import EditNoteForm from "./EditNoteForm.vue";
+import {useEditModal} from "../store/edit-modal-store.ts";
+
+const editModalStore = useEditModal()
+</script>
+
+<template>
+  <Modal :close-modal="editModalStore.closeEditModal">
+    <EditNoteForm/>
+  </Modal>
+</template>
+
+<style scoped>
+
+</style>
