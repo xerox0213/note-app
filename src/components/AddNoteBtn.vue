c8370e9c2b7c77d0fe4b82a60b2ac2db4b222efc
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-10-02 14:56:54 +0200

    feat: création du composant AddNoteBtn

---
+++ src/components/AddNoteBtn.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import {PlusIcon} from "@heroicons/vue/24/solid";
+</script>
+
+<template>
+  <button class="fixed right-8 bottom-8 p-2 bg-blue-500 rounded-lg transition-colors hover:bg-blue-600">
+    <PlusIcon class="size-9 text-white"/>
+  </button>
+</template>
+
+<style scoped>
+
+</style>
--- src/components/NoteApp.vue
+++ src/components/NoteApp.vue
@@ -2,12 +2,14 @@
 
 import Header from "./Header.vue";
 import Notes from "./Notes.vue";
+import AddNoteBtn from "./AddNoteBtn.vue";
 </script>
 
 <template>
   <Header/>
   <main class="pt-7 px-3">
     <Notes/>
+    <AddNoteBtn />
   </main>
 </template>
 
