948c6a9d3d887409958dc6dd525d8dc49e8fc8f8
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-10-02 20:57:32 +0200

    feat: création du composant Modal
    Permet d'avoir une modale réutilisable dans n'importe quelle cirsconstance

---
+++ src/components/Modal.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+type Props = { closeModal: () => void }
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <div class="fixed z-50 inset-0 flex items-center justify-center px-3">
+    <div @click="closeModal" class="absolute inset-0 bg-black opacity-20 -z-10"></div>
+    <slot/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
