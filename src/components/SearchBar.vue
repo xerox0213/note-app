3ab10c17ea3dc18984bb21082549b7e834e1680f
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-10-03 13:04:54 +0200

    feat: ajout de la fonctionnalité de recherche de notes

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
 import {MagnifyingGlassIcon} from "@heroicons/vue/24/outline"
+import {useNote} from "../store/note-store.ts";
+import {ref, watchEffect} from "vue";
+
+const noteStore = useNote()
+const input = ref<string>("")
+
+watchEffect(() => noteStore.makeSearch(input.value))
 </script>
 
 <template>
   <div class="flex items-center bg-gray-100 px-4 py-2 rounded">
-    <input type="text" placeholder="Search notes" class="grow bg-transparent rounded outline-0"/>
+    <input v-model="input" type="text" placeholder="Search notes"
+           class="grow bg-transparent rounded outline-0"/>
     <MagnifyingGlassIcon class="size-4 text-gray-500"/>
   </div>
 </template>
